9ea4ba4314704eada96a066a74d74d3d451c44b4
spencer28 <king44c@gmail.com>
2020-08-28 23:49:32 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  Graph G;
+  Matching M;
+  read_graph(G);
+  compute_matching(G, M);
+  write_matching(M);
+}
+
+void comute_matching(graph& G, matching& M) {
+
+
+
+
+}
