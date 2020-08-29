80d244d0acd2ec5bfbb8d6585c97711ed5da313c
spencer28 <king44c@gmail.com>
2020-08-29 21:00:35 +0000

    find_augmenting_path definition

--- main.cpp
+++ main.cpp
@@ -20,7 +20,20 @@ void comute_matching(graph& G, matching& M) {
 bool find_augmenting_path(graph& G, matching M) {
 bool found_it = false;
 while (!found_it) {
-
+  Edge min_edge;
+  pseudo_node min_red_z;
+  int bb_min = find_bb_min(G, min_edge);
+  int bu_min = find_bu_min(G, min_edge);
+  int z_min - find_z_min(G, min_red_z);
+  if (bu_min <= bb_min && bu_min <= z_min {
+     uncolored_2_blue(min_edge, G);
+  } else if (bb_min <= bu_min && bb_min <= z_min) {
+    found_it = augment_path_or_collapse_blossom(min_edge, G, M)
+  } else {
+    expand_pseudo_node(min_red_z);
+  }
+  }
+}
 
 
 }
