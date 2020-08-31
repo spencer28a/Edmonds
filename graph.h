b3aeb3c07d28aea309f60d7202d5fd06a5653215
spencer28 <king44c@gmail.com>
2020-08-31 22:31:07 +0000

    mode code 3

---
+++ graph.h
@@ -0,0 +1,41 @@
+#pragma once;
+// pure vitual class representing a vertex in the graph with blossoms collapsed.
+class Node {
+
+
+  
+};
+
+// a vertex in the original graph (no blossoms collapsed)
+class Vertex : public Node {
+
+
+
+
+};
+
+// a collapsed blossom
+class PseudoNode : public Node {
+
+
+
+};
+
+class edge {
+
+
+
+};
+
+class Graph {
+
+
+
+};
+
+
+class Matching {
+
+
+
+};
